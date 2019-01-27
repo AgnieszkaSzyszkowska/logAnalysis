6215961b9dacd2569c905cf91c7565347035c98b
Agnieszka <aszyszkowska93@gmail.com>
2019-01-27 22:25:09 +0100

    Logging improved in LogAnalyzer.java

--- src/main/java/com/interview/loganalysis/log/analyze/LogAnalyzer.java
+++ src/main/java/com/interview/loganalysis/log/analyze/LogAnalyzer.java
@@ -1,6 +1,8 @@
 package com.interview.loganalysis.log.analyze;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -15,6 +17,9 @@ class LogAnalyzer {
     private InputLogData inputLogData;
     private AlertedLogsStorage alertedLogsStorage;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogAnalyzer.class);
+
+
     void startAnalysis() throws IOException {
 
         final Map<String, Log> mapOfLogs = new ConcurrentHashMap<>();
@@ -25,19 +30,27 @@ class LogAnalyzer {
                     if (InputLogState.STARTED.equals(inputLog.getState())) {
                         final Optional<Log> logToClean = Optional.ofNullable(mapOfLogs.get(inputLog.getUniqueId()));
                         if (logToClean.isPresent()) {
+                            LOGGER.debug("Second input for id: {}", logToClean.get().getUniqueId());
                             logToClean.get().setStartTimestampAndAlertIfApplicable(inputLog.getTimestamp());
                             alertedLogsStorage.storeAlertedLogs(logToClean.get());
+                            LOGGER.debug("Log with id: {} stored", inputLog.getUniqueId());
                             mapOfLogs.remove(inputLog.getUniqueId());
+                            LOGGER.debug("Id: {} removed from map", inputLog.getUniqueId());
                         } else {
+                            LOGGER.debug("First input for id: {}", inputLog.getUniqueId());
                             mapOfLogs.put(inputLog.getUniqueId(), createNewLogWithStartDate(inputLog));
                         }
                     } else if (InputLogState.FINISHED.equals(inputLog.getState())) {
                         final Optional<Log> logToClean = Optional.ofNullable(mapOfLogs.get(inputLog.getUniqueId()));
                         if (logToClean.isPresent()) {
+                            LOGGER.debug("Second input for id: {}", logToClean.get().getUniqueId());
                             logToClean.get().setEndTimestampAndAlertIfApplicable(inputLog.getTimestamp());
                             alertedLogsStorage.storeAlertedLogs(logToClean.get());
+                            LOGGER.debug("Log with id: {} stored", inputLog.getUniqueId());
                             mapOfLogs.remove(inputLog.getUniqueId());
+                            LOGGER.debug("Id: {} removed from map", inputLog.getUniqueId());
                         } else {
+                            LOGGER.debug("First input for id: {}", inputLog.getUniqueId());
                             mapOfLogs.put(inputLog.getUniqueId(), createNewLogWithEndDate(inputLog));
                         }
                     }
